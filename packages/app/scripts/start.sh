fc939995d485d37f082b436411d26cca756f7d42
michaelcueno <michael.cueno@convoy.com>
2023-05-05 08:13:48 -0700

    make top level `yarn start` just work

---
+++ packages/app/scripts/start.sh
@@ -0,0 +1,6 @@
+export REACT_APP_API_AUDIENCE="http://localhost:3000" 
+export REACT_APP_PLATFORM_URL="http://localhost:3000" 
+export REACT_APP_API_URL="http://localhost:3000/graphql" 
+export PORT=3001
+
+NODE_OPTIONS=--openssl-legacy-provider yarn react-scripts start
